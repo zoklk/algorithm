34bd3cb16d16c2a7afafddd02b2b2fde211e00ba
zoklk <bjincheol34@gmail.com>
2025-04-03 16:55:18 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 520 ms, Memory: 36788 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -0,0 +1,75 @@
+// 10867번: 중복 빼고 정렬하기
+// <새롭게 배운 포인트>
+
+// <문제 이해>
+// 1. testcase 100,000이니까 NlogN
+// 2. 입력받은거 mergesort + 중복 제외 출력
+
+// <구체적 예제>
+
+// <단계 나누기>
+// 1. N을 입력받는다.
+// 2. 숫자를 입력받아서 arr에 int로 파싱
+// 3. arr를 정렬한다.
+// 4. for를 이용하여 중복을 제외하고 출력한다.
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) throws IOException{
+    // 1. N과 배열을 입력받는다.
+    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    int n=Integer.parseInt(br.readLine());
+    String[] token=br.readLine().trim().split(" ");
+
+    // 2. 숫자를 입력받아서 arr에 int로 파싱
+    int[] arr = new int[n];
+    for(int i=0; i<n; i++){
+      arr[i]=Integer.parseInt(token[i]);
+    }
+
+    // 3. arr를 정렬한다.
+    quickSort(arr, 0, n-1);
+
+    // 4. for를 이용하여 중복을 제외하고 출력한다.
+    int key=-1001;
+    for(int i=0; i<n; i++){
+      if(key != arr[i]){
+        System.out.printf("%d ", arr[i]);
+        key=arr[i];
+      }
+    }
+  }
+
+  // quick sort 구현
+  public static void quickSort(int[] arr, int left, int right){
+    if(left>=right){
+      return;
+    }
+    int pi=partition(arr, left, right);
+    quickSort(arr,left,pi-1);
+    quickSort(arr,pi,right);
+  }
+
+  public static int partition(int[] arr, int left, int right){
+    int pivot=arr[(left+right)/2];
+    
+    while(left<=right){
+      while(arr[left]<pivot) left++;          // arr[left]<=pivot 으로 할 경우 [5, 5, 5, 5, 5]같은 배열에서 idx를 넘을 수 있다.
+      while(arr[right]>pivot) right--;
+      if(left<=right){
+        swap(arr,left,right);
+        left++;
+        right--;
+      }
+    }
+    return left;
+  }
+
+  public static void swap(int[] arr, int left, int right){
+    int temp=arr[left];
+    arr[left]=arr[right];
+    arr[right]=temp;
+  }
+}
