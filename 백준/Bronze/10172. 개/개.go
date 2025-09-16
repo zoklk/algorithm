4c87e72e53d9e727f7b10d5b89485b0cee586818
Byun Jincheol <bjincheol34@gmail.com>
2025-09-16 11:47:17 +0900

    [Bronze V] Title: 개, Time: 4 ms, Memory: 772 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("|\\_/|\n")
+	fmt.Printf("|q p|   /}\n")
+	fmt.Printf("( 0 )\"\"\"\\\n")
+	fmt.Printf("|\"^\"`    |\n")
+	fmt.Printf("||_/=\\\\__|\n")
+}
